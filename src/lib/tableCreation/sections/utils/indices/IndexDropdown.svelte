f6c1d3a258403594a4ea222602625d866e5e49a9
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-10-19 12:21:11 +0100

    Add styling to index dropdown & make it more-or-less functional

--- src/lib/tableCreation/sections/utils/indices/IndexDropdown.svelte
+++ src/lib/tableCreation/sections/utils/indices/IndexDropdown.svelte
@@ -10,6 +10,17 @@
     export let indexCols: string[]
 
     const dispatch = createEventDispatcher<{ close: void }>()
+
+    function toggleIndexColPresent(col: string): void {
+        const i = indexCols.indexOf(col)
+
+        if (i == -1) {
+            indexCols = [...indexCols, col]
+        } else {
+            indexCols.splice(i, 1)
+            indexCols = indexCols
+        }
+    }
 </script>
 
 <BgCloser {dispatch} />
@@ -17,19 +28,76 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="wrapper" on:click={(e) => e.stopPropagation()}>
+    <!-- TODO: Use this input to filter visible columns -->
     <Input />
     <Separator />
 
-    {#each allColNames as col}
-        <div class="col-data row">
-            <input type="checkbox" />
-            <p>{col}</p>
-        </div>
-    {/each}
+    <div class="col all-cols">
+        {#each allColNames as col}
+            <label class="col-data row">
+                <input type="checkbox" checked={indexCols.includes(col)} on:change={() => toggleIndexColPresent(col)} />
+                <p>{col}</p>
+            </label>
+        {/each}
+    </div>
 </div>
 
 <style lang="scss">
     @use '$lib/utils/multi_index' as ind;
+
+    label {
+        cursor: pointer;
+    }
+
+    .all-cols {
+        gap: 6px;
+        padding: 6px;
+    }
+
+    .col-data {
+        $fs: 1rem;
+        $check-size: calc($fs * 0.8);
+        $inner-check-size: calc($check-size * 0.4);
+
+        gap: 4px;
+        font-size: $fs;
+        color: $primary;
+        align-items: center;
+
+        p {
+            font-size: inherit;
+            color: inherit;
+        }
+
+        input {
+            display: grid;
+            place-content: center;
+            appearance: none;
+            margin: 0;
+            color: inherit;
+            width:  $check-size;
+            height: $check-size;
+
+            border: 2px solid currentColor;
+            border-radius: 6px;
+
+            &::before {
+                content: '';
+                display: block;
+                width: $inner-check-size;
+                height: $inner-check-size;
+                transform: scale(0);
+                transition: 0.2s transform;
+                background: $primary;
+                border-radius: 99px;
+            }
+
+            &:checked::before {
+                transform: scale(1);
+            }
+        }
+    }
+
 
     .wrapper {
         width: calc(100% - ind.$wrapperPadding * 2);
