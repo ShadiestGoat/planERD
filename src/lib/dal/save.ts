9362515c63a6e585eae94e717ed395d690d12d97
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-10-22 05:55:35 +0100

    Add persistent relations

--- src/lib/dal/save.ts
+++ src/lib/dal/save.ts
@@ -1,7 +1,7 @@
-import type { Node, XYPosition } from '@xyflow/svelte'
-import type { Index, Table } from '$lib/types'
-import { nodes } from './nodes'
-import { indices, multiColIndexExceptions, tableOrder, tables } from './data'
+import type { XYPosition } from '@xyflow/svelte'
+import type { Index, Relation, Table } from '$lib/types'
+import { edges, nodes } from './nodes'
+import { indices, multiColIndexExceptions, relations, tableOrder, tables } from './data'
 import { get } from 'svelte/store'
 import { tableNode } from './api'
 
@@ -11,8 +11,8 @@ export enum StorageKeys {
     POS = 'xy',
     TABLES = 'tables',
     INDICES = 'indices',
-    MULTI_COL_EXCEPTIONS = 'multi_col_exceptions'
-    // RELATIONS = 'relations'
+    MULTI_COL_EXCEPTIONS = 'multi_col_exceptions',
+    RELATIONS = 'relations'
 }
 
 type StorageData = {
@@ -20,80 +20,52 @@ type StorageData = {
     [StorageKeys.TABLES]: Table[]
     [StorageKeys.INDICES]: Record<string, Index[]>
     [StorageKeys.MULTI_COL_EXCEPTIONS]: Record<string, number[]>
-    // [StorageKeys.RELATIONS]: Relation[],
+    [StorageKeys.RELATIONS]: Relation[]
 }
 
-function savePos(allNodes: Node[]): void {
-    localStorage.setItem(
-        StorageKeys.POS,
-        JSON.stringify(
-            Object.fromEntries(
-                allNodes.map((n) => {
-                    return [`${n.type}-${n.id}`, n.position]
-                })
-            )
+type ParsingFunc<T, D> = (v: T) => D
+
+function genericSave<
+    T,
+    K extends StorageKeys,
+    D = StorageData[K],
+    F = T extends D ? undefined : ParsingFunc<T, D>
+>(k: K, parse?: F): (v: T) => void {
+    return (v) => {
+        if (!dataLoaded) return
+
+        setTimeout(() => {
+            const d = parse ? (parse as ParsingFunc<T, D>)(v) : (v as unknown as D)
+            localStorage.setItem(k, JSON.stringify(d))
+        }, 1)
+    }
+}
+
+nodes.subscribe(
+    genericSave(StorageKeys.POS, (v) => {
+        return Object.fromEntries(
+            v.map((n) => {
+                return [`${n.type}-${n.id}`, n.position]
+            })
         )
+    })
+)
+
+function parseTables(order: string[], tableData: Record<string, Table>): Table[] {
+    return order.map((n) => tableData[n])
+}
+
+tableOrder.subscribe(genericSave(StorageKeys.TABLES, (v) => parseTables(v, get(tables))))
+tables.subscribe(genericSave(StorageKeys.TABLES, (v) => parseTables(get(tableOrder), v)))
+
+indices.subscribe(genericSave(StorageKeys.INDICES))
+relations.subscribe(genericSave(StorageKeys.RELATIONS))
+
+multiColIndexExceptions.subscribe(
+    genericSave(StorageKeys.MULTI_COL_EXCEPTIONS, (v) =>
+        Object.fromEntries(Object.keys(v).map((k) => [k, Array.from(v[k])]))
     )
-}
-
-export function saveTables(order: string[], tables: Record<string, Table>): void {
-    const d = order.map((n) => tables[n])
-
-    localStorage.setItem(StorageKeys.TABLES, JSON.stringify(d))
-}
-
-export function saveIndices(ind: Record<string, Index[]>): void {
-    localStorage.setItem(StorageKeys.INDICES, JSON.stringify(ind))
-}
-
-export function saveMultiColException(exceptions: Record<string, Set<number>>): void {
-    localStorage.setItem(
-        StorageKeys.MULTI_COL_EXCEPTIONS,
-        JSON.stringify(
-            Object.fromEntries(Object.keys(exceptions).map((k) => [k, Array.from(exceptions[k])]))
-        )
-    )
-}
-
-nodes.subscribe((v) => {
-    if (!dataLoaded) return
-
-    setTimeout(() => {
-        savePos(v)
-    }, 1)
-})
-
-tableOrder.subscribe((v) => {
-    if (!dataLoaded) return
-
-    setTimeout(() => {
-        saveTables(v, get(tables))
-    }, 1)
-})
-
-tables.subscribe((v) => {
-    if (!dataLoaded) return
-
-    setTimeout(() => {
-        saveTables(get(tableOrder), v)
-    }, 1)
-})
-
-indices.subscribe((v) => {
-    if (!dataLoaded) return
-
-    setTimeout(() => {
-        saveIndices(v)
-    }, 1)
-})
-
-multiColIndexExceptions.subscribe((v) => {
-    if (!dataLoaded) return
-
-    setTimeout(() => {
-        saveMultiColException(v)
-    }, 1)
-})
+)
 
 async function readLocalStorageAsync<K extends StorageKeys>(
     k: K,
@@ -108,11 +80,12 @@ async function readLocalStorageAsync<K extends StorageKeys>(
 export async function loadData(): Promise<void> {
     if (dataLoaded) return
 
-    const [dataPos, dataIndex, dataTable, exceptions] = await Promise.all([
+    const [dataPos, dataIndex, dataTable, exceptions, dataRelations] = await Promise.all([
         readLocalStorageAsync(StorageKeys.POS, {}),
         readLocalStorageAsync(StorageKeys.INDICES, {}),
         readLocalStorageAsync(StorageKeys.TABLES, []),
-        readLocalStorageAsync(StorageKeys.MULTI_COL_EXCEPTIONS, {})
+        readLocalStorageAsync(StorageKeys.MULTI_COL_EXCEPTIONS, {}),
+        readLocalStorageAsync(StorageKeys.RELATIONS, [])
     ])
 
     const order = dataTable.map((v) => v.name)
@@ -146,6 +119,19 @@ export async function loadData(): Promise<void> {
 
     multiColIndexExceptions.set(
         Object.fromEntries(Object.keys(exceptions).map((k) => [k, new Set(exceptions[k])]))
+    )
+    relations.set(dataRelations)
+
+    edges.set(
+        dataRelations.map((v) => {
+            return {
+                id: `${v.from} -> ${v.to}`,
+                source: v.from.split(' ')[0],
+                target: v.to.split(' ')[0],
+                sourceHandle: `${v.from} sl`,
+                targetHandle: `${v.to} tl`
+            }
+        })
     )
 
     dataLoaded = true
